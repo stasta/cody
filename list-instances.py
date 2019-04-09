2930cb65789f9c824f3dc6adb8b9cf024781d085
Gustavo <gustavoofernandes@gmail.com>
2019-04-08 18:46:39 -0700

    Adding profile and region to script

--- list-instances.py
+++ list-instances.py
@@ -1,7 +1,17 @@
+#!/usr/local/bin/python3
 import boto3
 from termcolor import colored
 
+AWS_REGION_NAME='us-east-1'
+AWS_PROFILE_NAME='cody'
+
+boto3.setup_default_session(profile_name=AWS_PROFILE_NAME, region_name=AWS_REGION_NAME)
 ec2 = boto3.resource('ec2')
+
+instances = ec2.instances.filter(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+for instance in instances:
+    print(instance.id, instance.instance_type)
 
 for i in ec2.instances.all():
 
