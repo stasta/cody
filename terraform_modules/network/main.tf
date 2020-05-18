1a9c6d594f3949943fe62636b21cbeea6df65a1e
Gustavo Fernandes <gustavo@fispan.com>
2020-05-17 19:02:55 -0700

    Adding subnets name tags

--- terraform_modules/network/main.tf
+++ terraform_modules/network/main.tf
@@ -9,19 +9,41 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "primary_private_subnet" {
   cidr_block = "${var.subnet_cidr_block["primary_private"]}"
   vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "primary_private"
+  }
 }
 
 resource "aws_subnet" "secondary_private_subnet" {
   cidr_block = "${var.subnet_cidr_block["secondary_private"]}"
   vpc_id = "${aws_vpc.vpc.id}"
-}
+
+  tags = {
+    Name = "secondary_private"
+  }}
 
 resource "aws_subnet" "primary_public_subnet" {
   cidr_block = "${var.subnet_cidr_block["primary_public"]}"
   vpc_id = "${aws_vpc.vpc.id}"
-}
+
+  tags = {
+    Name = "primary_public"
+  }}
 
 resource "aws_subnet" "secondary_public_subnet" {
   cidr_block = "${var.subnet_cidr_block["secondary_public"]}"
   vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "secondary_public"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
 }
