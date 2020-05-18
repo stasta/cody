18ff0aead32f1ef412f48d1d3ec7c573c7416238
Gustavo Fernandes <gustavo@fispan.com>
2020-05-17 19:36:44 -0700

    Enabling the mapping of public IPs; Creating public routes and associations to public subnets

--- terraform_modules/network/main.tf
+++ terraform_modules/network/main.tf
@@ -18,6 +18,7 @@ resource "aws_subnet" "primary_public_subnet" {
   cidr_block = "${var.subnet_cidr_block["primary_public"]}"
   vpc_id = "${aws_vpc.vpc.id}"
   availability_zone = "${data.aws_availability_zones.azs.names[0]}"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-${data.aws_availability_zones.azs.names[0]}"
@@ -27,6 +28,7 @@ resource "aws_subnet" "secondary_public_subnet" {
   cidr_block = "${var.subnet_cidr_block["secondary_public"]}"
   vpc_id = "${aws_vpc.vpc.id}"
   availability_zone = "${data.aws_availability_zones.azs.names[1]}"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-${data.aws_availability_zones.azs.names[1]}"
@@ -39,4 +41,27 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "${var.vpc_name}-igw"
   }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.igw.id}"
+}
+
+resource "aws_route_table_association" "primary_public_subnet_route_table_association" {
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  subnet_id = "${aws_subnet.primary_public_subnet.id}"
+}
+
+resource "aws_route_table_association" "secondary_public_subnet_route_table_association" {
+  route_table_id = "${aws_route_table.public_route_table.id}"
+  subnet_id = "${aws_subnet.secondary_public_subnet.id}"
 }
