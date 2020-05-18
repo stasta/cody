b11608f7021487655decac90aa8d53884fd8b6b0
Gustavo Fernandes <gustavo@fispan.com>
2020-05-17 20:26:49 -0700

    Enabling DNS support on VPC

--- terraform_modules/network/main.tf
+++ terraform_modules/network/main.tf
@@ -8,6 +8,8 @@ data "aws_availability_zones" "azs" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr_block}"
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = "${var.vpc_name}"
