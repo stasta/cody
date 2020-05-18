f12242d50e8d8079ff4a61cb007f0dccbb18799f
Gustavo Fernandes <gustavo@fispan.com>
2020-05-17 19:39:05 -0700

    Adding network output

--- terraform_modules/network/output.tf
+++ terraform_modules/network/output.tf
@@ -1,1 +1,11 @@
+output "vpc" {
+  value = "${aws_vpc.vpc.id}"
+}
 
+output "primary_public_subnet" {
+  value = "${aws_subnet.primary_public_subnet.id}"
+}
+
+output "secondary_public_subnet" {
+  value = "${aws_subnet.secondary_public_subnet.id}"
+}
