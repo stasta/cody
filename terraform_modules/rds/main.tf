8087ea983c8e7340ed4b84dd53d4d56fce57ad9e
Gustavo Fernandes <gustavo@fispan.com>
2020-05-21 11:31:09 -0700

    Adding RDS instance

--- terraform_modules/rds/main.tf
+++ terraform_modules/rds/main.tf
@@ -1,1 +1,13 @@
+resource "aws_db_instance" "database_instance" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
 
+  skip_final_snapshot  = true
+}
