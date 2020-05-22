f0df68358f254da939f7b4df6d92345993d07340
Gustavo Fernandes <gustavo@fispan.com>
2020-05-21 22:27:16 -0700

    Fixing db subnet

--- terraform_modules/rds/main.tf
+++ terraform_modules/rds/main.tf
@@ -1,15 +1,21 @@
 resource "aws_db_instance" "db_instance" {
-  allocated_storage    = 5
+  identifier_prefix = "wordpress-db" //TODO
+
+  allocated_storage    = 5 //TODO
   storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  engine               = "mysql" //TODO
+  engine_version       = "5.7" //TODO
+  instance_class       = "db.t2.micro" //TODO
+  name                 = "mydb" //TODO
+  username             = "foo" //TODO
+  password             = "foobarbaz" //TODO
   parameter_group_name = "default.mysql5.7"
 
-  //  skip_final_snapshot  = true
+  db_subnet_group_name = "${aws_db_subnet_group.db_subnet_group.name}"
+  publicly_accessible = true
+
+  skip_final_snapshot  = true
+  final_snapshot_identifier = "ignore"
 }
 
 resource "aws_db_subnet_group" "db_subnet_group" {
