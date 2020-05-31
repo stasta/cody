47646825252378ca726302782067eb82aaa7e32e
Gustavo Fernandes <gustavo@fispan.com>
2020-05-30 23:37:19 -0700

    adding remove reminders

--- terraform_modules/network/output.tf
+++ terraform_modules/network/output.tf
@@ -2,10 +2,12 @@ output "vpc" {
   value = "${aws_vpc.vpc.id}"
 }
 
+//TODO remove
 output "primary_public_subnet" {
   value = "${aws_subnet.primary_public_subnet.id}"
 }
 
+//TODO remove
 output "secondary_public_subnet" {
   value = "${aws_subnet.secondary_public_subnet.id}"
 }
